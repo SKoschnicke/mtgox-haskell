8d099afc22377499469e574da2eddb562a77d123
Robin Krom <robin.krom@math.ethz.ch>
2013-01-21 11:32:39 +0100

    initial commit.

---
+++ Mtgox.hs
@@ -0,0 +1,233 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Mtgox (
+apiHost,
+apiPort,
+connect,
+splitHeader,
+frame,
+unframe,
+GoxMessage (..),
+SubscribeMsg (..),
+UnsubscribeMsg (..),
+RemarkMsg (..),
+PrivateMsg (..),
+PrivateMsgType (..),
+ResultMsg (..),
+TickerMsg (..),
+TickerPrice (..),
+TradeMsg (..),
+DepthMsg (..),
+TradeType (..)
+)
+where
+
+
+import Data.Aeson
+import Control.Applicative
+import Control.Monad
+import Network.Socket (PortNumber)
+import qualified Data.ByteString.Char8 as BC
+import System.Certificate.X509
+import qualified Data.ByteString.Lazy.Char8 as LC
+import Network.TLS
+import Data.IORef
+import TLS
+
+apiHost :: String
+apiHost = "socketio.mtgox.com"
+
+apiPort :: PortNumber
+apiPort = 443
+
+data GoxMessage = S SubscribeMsg | US UnsubscribeMsg | Rem RemarkMsg | P PrivateMsg | R ResultMsg deriving (Show, Eq)
+
+instance FromJSON GoxMessage where
+	parseJSON v@(Object _) = (S <$> parseJSON v) <|> (US <$> parseJSON v) <|> (Rem <$> parseJSON v) <|> (P <$> parseJSON v) <|> (R <$> parseJSON v)
+	parseJSON _ = mzero
+ 
+data SubscribeMsg = SubscribeMsg { s_channel :: String} deriving (Show, Eq)
+
+instance FromJSON SubscribeMsg where
+	parseJSON (Object v) = v .: "op" >>= withText "op" (\str -> if str == "subscribe" 
+																then SubscribeMsg <$> v .: "channel"
+																else mzero)
+	parseJSON _ = mzero
+
+data UnsubscribeMsg = UnsubscribeMsg { u_channel :: String } deriving (Show, Eq)
+
+instance FromJSON UnsubscribeMsg where
+	parseJSON (Object v) = v .: "op" >>= withText "op" (\str -> if str == "unsubscribe"
+																then UnsubscribeMsg <$> v .: "channel"
+																else mzero)
+	parseJSON _ = mzero
+
+data RemarkMsg = RemarkMsg {message :: String, success :: Bool} deriving (Show, Eq)
+
+instance FromJSON RemarkMsg where
+	parseJSON (Object v) = v .: "op" >>= withText "op" (\str -> if str == "remark" 
+																then RemarkMsg <$> v .: "message" <*> v .: "success"
+																else mzero)
+	parseJSON _ = mzero
+
+data PrivateMsg = PrivateMsg {p_channel :: String, p_origin :: String, p_private :: PrivateMsgType} deriving (Show, Eq)
+
+instance FromJSON PrivateMsg where
+	parseJSON o@(Object v) = v .: "op" >>= withText "op" (\str -> if str == "private"
+																then PrivateMsg <$> v .: "channel" <*> v .: "origin" <*> parseJSON o
+																else mzero) 
+	parseJSON _ = mzero
+
+data PrivateMsgType = T TickerMsg | Tr TradeMsg | D DepthMsg | Rs ResultMsg deriving (Show, Eq)
+
+instance FromJSON PrivateMsgType where
+	parseJSON v@(Object _) = (T <$> parseJSON v) <|> (Tr <$> parseJSON v) <|> (D <$> parseJSON v) <|> (Rs <$> parseJSON v)
+	parseJSON _ = mzero
+
+data ResultMsg = ResultMsg { id :: String, result :: String } deriving (Show, Eq)
+
+instance FromJSON ResultMsg where
+	parseJSON (Object v) = v .: "op" >>= withText "op" (\str -> if str == "result" 
+																then ResultMsg <$> v .: "id" <*> v .: "result"
+																else mzero) 
+	parseJSON _ = mzero
+
+data TickerMsg = TickerMsg {
+	avg :: TickerPrice,
+	buy :: TickerPrice,
+	sell :: TickerPrice,
+	high :: TickerPrice,
+	low :: TickerPrice,
+	vol :: TickerPrice,
+	last :: TickerPrice,
+	last_local :: TickerPrice,
+	last_orig :: TickerPrice,
+	last_all :: TickerPrice,
+	vwap :: TickerPrice
+	} deriving (Show, Eq)
+
+instance FromJSON TickerMsg where
+	parseJSON (Object v) = let h s = v .: "ticker" >>= (\o -> o .: s)	
+		in TickerMsg <$> h "avg"
+						<*> h "buy"
+						<*> h "sell"
+						<*> h "high"
+						<*> h "low"
+						<*> h "vol"
+						<*> h "last"
+						<*> h "last_local"
+						<*> h "last_orig"
+						<*> h "last_all"
+						<*> h "vwap"
+	parseJSON _ = mzero
+
+data TickerPrice = TickerPrice {
+	tip_currency :: String,
+	display :: String,
+	value :: Double,
+	value_int :: Integer
+} deriving (Show, Eq)
+
+instance FromJSON TickerPrice where
+	parseJSON (Object v) = TickerPrice <$> v .: "currency" 
+										<*> v .: "display"
+										<*> (read <$> v .: "value")
+										<*> (read <$> v .: "value_int")
+	parseJSON _ = mzero
+
+data TradeMsg = TradeMsg {
+	amount :: Double,
+	amount_int :: Integer,
+	date :: Integer,
+	tr_item :: String,
+	tr_price :: Double,
+	price_currency :: String,
+	tr_price_int :: Integer,
+	primary :: String,
+	properties :: String,
+	tid :: Integer,
+	trade_type :: TradeType,
+	tr_type :: String
+	} deriving (Show, Eq)
+
+instance FromJSON TradeMsg where
+	parseJSON (Object v) = let h s = v .: "trade" >>= (\o -> o .: s)
+		in TradeMsg <$> h "amount"
+					<*> (read <$> h "amount_int")
+					<*> h "date"
+					<*> h "item"
+					<*> h "price"
+					<*> h "price_currency"
+					<*> (read <$> h "price_int")
+					<*> h "primary"
+					<*> h "properties"
+					<*> (read <$> h "tid")
+					<*> h "trade_type"
+					<*> h "type"
+	parseJSON _ = mzero
+
+data DepthMsg = DepthMsg {
+	d_currency :: String,
+	d_item :: String,
+	now :: Integer,
+	d_price :: Double,
+	d_price_int :: Integer,
+	total_volume_int :: Integer,
+	d_type :: Int,
+	type_str :: TradeType,
+	volume :: Double,
+	volume_int :: Integer
+} deriving (Show, Eq)
+
+instance FromJSON DepthMsg where
+	parseJSON (Object v) = let h s = v .: "depth" >>= (\o -> o .: s)
+		in DepthMsg <$> h "currency"
+					<*> h "item"
+					<*> (read <$> h "now")
+					<*> (read <$> h "price")
+					<*> (read <$> h "price_int")
+					<*> (read <$> h "total_volume_int")
+					<*> h "type"
+					<*> h "type_str"
+					<*> (read <$> h "volume")
+					<*> (read <$> h "volume_int")
+	parseJSON _ = mzero
+
+data TradeType = Bid | Ask deriving (Show, Eq)
+
+instance FromJSON TradeType where
+	parseJSON (String s) = if s == "bid" then return Bid else return Ask 
+	parseJSON _ = mzero
+
+frame :: LC.ByteString -> LC.ByteString
+frame = ((flip LC.snoc) '\xff') . (LC.cons '\x00')
+
+unframe :: BC.ByteString -> BC.ByteString
+unframe = BC.init . BC.tail
+
+splitHeader :: BC.ByteString -> (BC.ByteString, BC.ByteString)
+splitHeader bs = BC.splitAt 3 $ unframe bs
+
+parseSid :: BC.ByteString -> String
+parseSid = takeWhile (/=':') . (!! 6) . lines . BC.unpack
+
+connect :: (Context -> IO a) -> IO a
+connect app = do 
+	certStore <- getSystemCertificateStore 
+	sStorage <- newIORef undefined
+	runTLS (getDefaultParams certStore sStorage Nothing) apiHost apiPort $ \ctx -> do
+			handshake ctx
+			sendData ctx $ LC.pack $ "GET /socket.io/1/ HTTP/1.1\r\n\r\n"
+			d <- recvData ctx
+			BC.putStrLn d
+			sendData ctx $ 
+				LC.pack $ 
+				"GET /socket.io/1/websocket/" ++ (parseSid d) ++ " HTTP/1.1\r\n" ++
+				"Upgrade: WebSocket\r\n" ++
+				"Connection: Upgrade\r\n" ++
+				"Host: socketio.mtgox.com\r\n" ++
+				"Origin: *\r\n\r\n"
+			d' <- recvData ctx
+			BC.putStrLn d'
+			sendData ctx $ frame $ LC.pack "1::/mtgox"
+			app ctx
---
+++ ticker.hs
@@ -0,0 +1,21 @@
+import qualified Data.ByteString.Lazy.Char8 as LC
+import qualified Data.ByteString.Char8 as BC
+import Control.Monad (forever)
+import Data.Aeson (decode)
+import Network.TLS
+import Mtgox
+
+ticker :: Context -> IO (Maybe GoxMessage)
+ticker ctx = do 
+		d <- recvData ctx
+		let (h,msg) = splitHeader d
+		case BC.unpack h of
+			-- send heartbeats back.
+			"2::" 	-> (sendData ctx $ frame $ LC.pack "2::") >> return Nothing
+			-- collect JSON messages.
+			"4::" 	-> return (decode $ LC.fromChunks [BC.drop 7 msg] :: Maybe GoxMessage)
+			-- ignore the rest.
+			_ -> return Nothing
+
+main :: IO ()
+main = connect $ (\c -> forever (ticker c >>= print)) 
