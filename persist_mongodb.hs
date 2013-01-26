e1238e7a828b65c640e6edc798b1ce46a1a15c7b
Yves Hauser <yves.hauser@gmail.com>
2013-01-26 18:19:46 +0100

    cosmetics: renaming

--- persist_mongodb.hs
+++ persist_mongodb.hs
@@ -7,16 +7,16 @@ import Network.TLS
 import Mtgox
 import Ticker hiding (main)
 
-m_persist :: Pipe -> Maybe GoxMessage -> IO ()
-m_persist pipe (Just (P (PrivateMsg _ _ (D d@(DepthMsg _ _ _ _ _ _ _ _ _ _))))) = m_insert pipe d >>= putStr . show 
-m_persist _ m = putStr $ show m
+persist :: Pipe -> Maybe GoxMessage -> IO ()
+persist pipe (Just (P (PrivateMsg _ _ (D d@(DepthMsg _ _ _ _ _ _ _ _ _ _))))) = insert' pipe d >>= putStr . show 
+persist _ m = putStr $ show m
 
-m_insert pipe d = access pipe master "mtgox" (insertDepth d) >>= putStr . show
+insert' pipe d = access pipe master "mtgox" (insertDepth d) >>= putStr . show
 	where insertDepth d = insert "depth" ["timestamp" =: (now d), "book" =: (d_item d), "currency" =: (d_currency d), "value" =: (d_price_int d), "volume" =: (volume_int d), "total_volume" =: (total_volume_int d), "value_type" =: (show $ type_str d)]
 
 mongo_conn :: Pipe -> Context -> IO ()
 mongo_conn pipe ctx = do m <- ticker ctx
-                         m_persist pipe m 
+                         persist pipe m 
                          mongo_conn pipe ctx
 
 main :: IO ()
