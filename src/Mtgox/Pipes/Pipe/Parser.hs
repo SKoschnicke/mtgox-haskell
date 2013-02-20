474a84d3d49ed2359de2e82e2d12da1286acc716
Yves Hauser <yves.hauser@gmail.com>
2013-02-20 13:40:04 +0100

    more generic

--- src/Mtgox/Pipes/Pipe/Parser.hs
+++ src/Mtgox/Pipes/Pipe/Parser.hs
@@ -7,8 +7,8 @@ import Control.Proxy
 
 import Data.Mtgox
 
-trans' :: (Proxy p) => () -> Pipe p String LC.ByteString IO ()
+trans' :: (Proxy p) => () -> Pipe p String LC.ByteString IO r
 trans' () = runIdentityP $ forever $ (request () >>= respond . LC.pack) 
 
-parse' :: (Proxy p) => () -> Pipe p LC.ByteString (Maybe GoxMessage) IO ()
+parse' :: (Proxy p) => () -> Pipe p LC.ByteString (Maybe GoxMessage) IO r
 parse' () = runIdentityP $ forever $ request () >>= respond . decode
