8e223e34819712703105dacb7d362c9e58825632
Yves Hauser <yves.hauser@gmail.com>
2013-02-21 20:30:47 +0100

    comments

--- src/Exec.hs
+++ src/Exec.hs
@@ -1,3 +1,6 @@
+{-| Collection of simple use cases in order to play around with 
+    Proxy (pipes library) composition 
+-}
 module Exec where
 
 import Control.Monad.Error
@@ -12,37 +15,53 @@ import Mtgox.Pipes.Producer.File
 import Mtgox.Pipes.Producer.Live
 import Mtgox.Pipes.Producer.RetrieveDepthMsg 
 
--- live stream
+-----------------
+-- live stream --
+-----------------
+
+-- | Parse and print messages from live feed to stdout
 exec1 :: IO ()
 exec1 = runSafeIO $ runProxy $ runEitherK $ 
     producerLive >-> tryK (parse >-> printD)
 
+-- | Parse and construct order book from live feed 
 exec2 :: IO ()
 exec2 = runSafeIO $ runProxy $ runEitherK $ evalStateK (OrderBook [] []) chain
     where chain :: CheckP p => () -> StateP OrderBook (EitherP SomeException p) a' a () C SafeIO r
           chain = mapP producerLive >-> \() -> hoistP try $ (parse >-> orderBookPrinter) ()
 
+-- | Parse and store live feed in a local mongodb
 exec3 :: IO ()
 exec3 = runSafeIO $ runProxy $ runEitherK $ 
     producerLive >-> tryK (parse >-> persistDB)
 
--- read files
+----------------
+-- read files --
+----------------
+
 file :: FilePath
 file = "../etc/DepthMsg.json"
 
+-- | Parse and print messages from file to stdout
 exec4 :: IO ()
 exec4 = runSafeIO $ runProxy $ runEitherK $ 
     readFileS file >-> tryK (parse >-> printD)
 
+-- | Parse and print messages from file to stdout twice (vertical composition)
 exec5 :: IO ()
 exec5 = runSafeIO $ runProxy $ runEitherK $ 
     (readFileS file >=> readFileS file) >-> tryK (parse >-> printD)
 
--- read database
+-------------------
+-- read database --
+-------------------
+
+-- | Print messages from mongodb to stdout
 exec6 :: IO ()
 exec6 = runErrorT go >>= either print print
     where go = runProxy $ producerDB >-> raiseK printD
 
+-- | Construct order book from mongodb and print it to stdout
 exec7 :: IO ()
 exec7 = runErrorT go >>= either print print
     where go = runProxy $ evalStateK (OrderBook [] []) $ producerDB >-> raiseK orderBookPrinter
