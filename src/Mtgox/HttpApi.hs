479c2f6acdce7765556b250b4b0c987d0b9d902e
Yves Hauser <yves.hauser@gmail.com>
2013-02-27 09:22:31 +0100

    cosmetics: small refactorings

--- src/Mtgox/HttpApi.hs
+++ src/Mtgox/HttpApi.hs
@@ -13,7 +13,6 @@ import System.Certificate.X509
 
 import Connection.TLS
 import Data.Mtgox
-
 import Mtgox.Pipes.Consumer.OrderBook 
 
 apiHost :: String
@@ -22,13 +21,15 @@ apiHost = "mtgox.com"
 apiPort :: PortNumber
 apiPort = 443
 
+-- | Fill OrderBook with fulldepth data from MtGox Http Api
 fillOrderBook :: IO OrderBook
 fillOrderBook = do o <- fulldepth 
                    case o of
                        Nothing -> return $ OrderBook [] []
-                       Just d -> let a = map extract $ fulldepth_asks d 
-                                     b = reverse $ map extract $ fulldepth_bids d in return $ OrderBook b a
+                       Just d -> let a = map extract . fulldepth_asks $ d 
+                                     b = reverse . map extract . fulldepth_bids $ d in return $ OrderBook b a
 
+-- | Helper needed for building the OrderBook
 extract :: Depth -> (Integer, Integer)
 extract d = (depth_price_int d, depth_amount_int d)
 
@@ -39,26 +40,24 @@ fulldepth = do
     sStorage <- newIORef undefined
     runTLS' (getDefaultParams certStore sStorage Nothing) apiHost apiPort get
     
+-- | Simple get for fulldepth and parsing of the response
 get :: Context -> IO (Maybe FullDepth)
-get ctx = do handshake ctx
-             sendData ctx $ LC.pack $ 
-                 "GET /api/1/BTCUSD/fulldepth HTTP/1.1\r\n" ++
-                 "User-Agent: tls-hs\r\n" ++
-                 "Accept: */*\r\n" ++
-                 "Host: mtgox.com\r\n\r\n"
-             -- headers
-             _ <- recvData ctx
-             -- body (Chunked transfer encoding)
-             b <- recvData ctx
-             res <- checkrec $ A.parse json b
-             return $ check res
-       where checkrec v = case v of
-                              A.Partial f -> do b <- recvData ctx 
-                                                checkrec $ f b 
-                              A.Done t r -> return $ A.Done t r
-                              _ -> undefined
-             check res = case res of
-                             A.Done _ v  -> case (fromJSON v :: Result FullDepth) of
-                                   Success a -> Just a
-                                   _         -> Nothing
-                             _           -> Nothing
+get ctx = do 
+    handshake ctx
+    sendData ctx $ LC.pack $
+        "GET /api/1/BTCUSD/fulldepth HTTP/1.1\r\n" ++
+        "User-Agent: tls-hs\r\n" ++
+        "Accept: */*\r\n" ++
+        "Host: mtgox.com\r\n\r\n"
+    -- headers
+    _ <- recvData ctx
+    -- body (Chunked transfer encoding)
+    b <- recvData ctx
+    parserec $ A.parse json b
+    where parserec v = case v of
+                           A.Partial p -> recvData ctx >>= parserec . p 
+                           A.Done _ v -> return $ parseval v
+                           f@(A.Fail _ _ _) -> print f >> error "parsing fulldepth via Http Api"
+          parseval v = case (fromJSON v :: Result FullDepth) of
+                           Success a -> Just a
+                           _         -> Nothing
