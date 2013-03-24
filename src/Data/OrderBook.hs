ea7e0d2725f2199c4c118b22bb312c1b726573c5
Robin Krom <robin.krom@math.ethz.ch>
2013-03-24 19:54:58 +0100

    fix: reexported the modules in Mtgox.hs main module.

--- src/Data/OrderBook.hs
+++ src/Data/OrderBook.hs
@@ -1,5 +1,5 @@
 module Data.OrderBook (
-      OrderBook(..)
+      OrderBook (..)
     , updateOrderBook
     )
     where
--- src/Mtgox.hs
+++ src/Mtgox.hs
@@ -1,9 +1,18 @@
-module Mtgox where
+module Mtgox (
+    module Mtgox.HttpApi,
+    module Data.OrderBook,
+    module Mtgox.Pipes.Consumer.PersistDepthMsg,
+    module Mtgox.Pipes.Pipe.Parser,
+    module Mtgox.Pipes.Producer.File,
+    module Mtgox.Pipes.Producer.Live,
+    module Mtgox.Pipes.Producer.RetrieveDepthMsg
+    )
+    where
 
 import Mtgox.HttpApi
-import Data.OrderBook 
-import Mtgox.Pipes.Consumer.PersistDepthMsg 
+import Data.OrderBook
+import Mtgox.Pipes.Consumer.PersistDepthMsg
 import Mtgox.Pipes.Pipe.Parser
 import Mtgox.Pipes.Producer.File
 import Mtgox.Pipes.Producer.Live
-import Mtgox.Pipes.Producer.RetrieveDepthMsg 
+import Mtgox.Pipes.Producer.RetrieveDepthMsg
